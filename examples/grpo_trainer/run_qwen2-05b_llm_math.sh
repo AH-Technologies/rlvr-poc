816c316ea8685fafb157ebc1ac3d55176fd19ee6
HaakonLauvrak <haakon.lauvrak@gmail.com>
2025-09-24 13:05:01 +0200

    Increase gpu used from 1 to 6

--- examples/grpo_trainer/run_qwen2-05b_llm_math.sh
+++ examples/grpo_trainer/run_qwen2-05b_llm_math.sh
@@ -44,7 +44,7 @@ python3 -m verl.trainer.main_ppo \
     trainer.project_name='verl_grpo_example_gsm8k' \
     trainer.experiment_name='qwen2_0.5b_function_rm' \
     trainer.n_gpus_per_node=1 \
-    trainer.nnodes=1 \
+    trainer.nnodes=6 \
     trainer.save_freq=20 \
     trainer.test_freq=5 \
     trainer.total_epochs=1 $@
