b31dd19ef1e9b0a5e47d82451d0a5e082e5e9848
HaakonLauvrak <haakon.lauvrak@gmail.com>
2025-09-24 12:43:08 +0200

    Reduce ppo min batch size to be less than batch size to avoid error

--- examples/grpo_trainer/run_qwen2-05b_llm_math.sh
+++ examples/grpo_trainer/run_qwen2-05b_llm_math.sh
@@ -22,7 +22,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.model.path=Qwen/Qwen2-0.5B-Instruct \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
-    actor_rollout_ref.actor.ppo_mini_batch_size=256 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=16 \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=16 \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=0.001 \
